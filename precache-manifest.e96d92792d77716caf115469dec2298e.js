021600cf67ac465ccf03385957438494c379e5d1
Mohamed Awde <kemoday@gmail.com>
2021-05-13 22:22:11 +0200

    Updates

---
+++ precache-manifest.e96d92792d77716caf115469dec2298e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00cd0bd88b5bc7138ae90a66e55bd48",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "9482e942665a3d5bd631",
+    "url": "/react-weather-app/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "724efda7c4974064991b",
+    "url": "/react-weather-app/static/css/main.ffdb8539.chunk.css"
+  },
+  {
+    "revision": "9482e942665a3d5bd631",
+    "url": "/react-weather-app/static/js/2.60e9da6b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-weather-app/static/js/2.60e9da6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724efda7c4974064991b",
+    "url": "/react-weather-app/static/js/main.65aff17a.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  }
+]);
